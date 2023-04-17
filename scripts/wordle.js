3d85dc19223168903049d12d7e0c4dfc60c46079
Thành Nguyễn <nhthanh719@gmail.com>
2023-04-17 21:28:33 +0700

    Change background-color and color for validated letters

---
+++ scripts/wordle.js
@@ -0,0 +1,3 @@
+//Get innerHTML of each score-board-letter
+//When pressing a letter button, the letter will be placed into the HTML places (pass the keys which are not letters)
+//When enter all 5 letters in a row, press Enter to valiate the work and return the result

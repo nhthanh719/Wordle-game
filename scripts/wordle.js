de93ac0a3f91614f0278e7a46e948dfb44fc49a8
Thành Nguyễn <nhthanh719@gmail.com>
2023-05-10 09:16:11 +0700

    move validate function code into submit function

--- scripts/wordle.js
+++ scripts/wordle.js
@@ -2,6 +2,7 @@ const letters = document.querySelectorAll(".score-board-letter");
 const loadingDiv = document.querySelector(".loader");
 const ANSWER_LENGTH = 5;
 const ROUNDS = 6;
+
 const init = async () => {
   let currentGuess = "";
   let currentRow = 0;
@@ -29,59 +30,69 @@ const init = async () => {
   };
 
   const submit = async () => {
-      if (currentGuess.length !== ANSWER_LENGTH) {
-        return;
+    if (currentGuess.length !== ANSWER_LENGTH) {
+      return;
+    }
+
+    setLoading(true);
+
+    const res = await fetch("https://words.dev-apis.com/validate-word", {
+      method: "POST",
+      body: JSON.stringify({ word: currentGuess }),
+    });
+    const resObj = await res.json();
+    const { validWord } = resObj;
+
+    setLoading(false);
+
+    if (!validWord) {
+      markInvalid();
+      return;
+    }
+
+    guessParts = currentGuess.split("");
+    const map = makeMap(wordParts);
+
+    // Mark each letter "correct", "close" or "wrong"
+    for (let i = 0; i < ANSWER_LENGTH; i++) {
+      //correct
+      if (guessParts[i] === wordParts[i]) {
+        letters[ANSWER_LENGTH * currentRow + i].classList.add("correct");
+        map[guessParts[i]]--;
       }
-
-      validateWord(currentGuess);
-
-      
-
-      guessParts = currentGuess.split("");
-      const map = makeMap(wordParts);
-
-      // Mark each letter "correct", "close" or "wrong"
-      for (let i = 0; i < ANSWER_LENGTH; i++) {
-        //correct
-        if (guessParts[i] === wordParts[i]) {
-          letters[ANSWER_LENGTH * currentRow + i].classList.add("correct");
-          map[guessParts[i]]--;
-        }
+    }
+    for (let i = 0; i < ANSWER_LENGTH; i++) {
+      //correct
+      if (guessParts[i] === wordParts[i]) {
+        //do nothing cause already did above
       }
-      for (let i = 0; i < ANSWER_LENGTH; i++) {
-        //correct
-        if (guessParts[i] === wordParts[i]) {
-          //do nothing cause already did above
-        }
-        //close
-        else if (wordParts.includes(guessParts[i]) && map[guessParts[i]] > 0) {
-          letters[ANSWER_LENGTH * currentRow + i].classList.add("close");
-          map[guessParts[i]]--;
-        }
-        //invalid
-        else {
-          letters[ANSWER_LENGTH * currentRow + i].classList.add("wrong");
-        }
+      //close
+      else if (wordParts.includes(guessParts[i]) && map[guessParts[i]] > 0) {
+        letters[ANSWER_LENGTH * currentRow + i].classList.add("close");
+        map[guessParts[i]]--;
       }
-      // TODO: validate word
-
-      //If win
-      if (currentGuess === word) {
-        window.alert("Congratulations");
-        done = true;
+      //invalid
+      else {
+        letters[ANSWER_LENGTH * currentRow + i].classList.add("wrong");
       }
+    }
 
-      //Go to next row & erase currentGuess
-      currentRow++;
-      currentGuess = "";
+    //If win
+    if (currentGuess === word) {
+      window.alert("Congratulations");
+      done = true;
+    }
 
-      //If lose
-      if (currentRow === ROUNDS) {
-        alert("You suck, sorry mate");
-        done = true;
-      }
-    };
-  
+    //Go to next row & erase currentGuess
+    currentRow++;
+    currentGuess = "";
+
+    //If lose
+    if (currentRow === ROUNDS) {
+      alert("You suck, sorry mate");
+      done = true;
+    }
+  };
 
   const backspace = () => {
     // remove last letter from current guess
@@ -112,20 +123,11 @@ const isLetter = (letter) => {
   return /^[a-zA-Z]$/.test(letter);
 };
 
-const validateWord = async (guessWord) => {
-  setLoading(true);
-  const res = await fetch("https://words.dev-apis.com/validate-word", {
-    method: "POST",
-    body: JSON.stringify({ word: guessWord }),
-  });
-  const resObj = await res.json();
-  const { validWord } = resObj;
-
-  setLoading(false);
-
-  if (!validWord) {
-    markInvalid();
-    return;
+const markInvalid = () => {
+  // alert("Not a valid word");
+  for (let i = 0; i < ANSWER_LENGTH; i++) {
+    console.log("Current letter is" + letters[i]);
+    letters[ANSWER_LENGTH * currentRow + i].classList.add("invalid");
   }
 };
 
